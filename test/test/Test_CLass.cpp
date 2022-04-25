46ffa3dd69e2c402260cab400d4ab351241804c6
Nathan0995 <92745512+Nathan0995@users.noreply.github.com>
2022-04-25 12:51:32 +0500

    Test_Class was added
    in here i have created two files one cpp and the other hpp for implementation as well as interface

---
+++ test/test/Test_CLass.cpp
@@ -0,0 +1,8 @@
+//
+//  Test_CLass.cpp
+//  test
+//
+//  Created by NURIDDINXO'JA ERKINXO'JAYEV on 25/04/22.
+//
+
+#include "Test_CLass.hpp"
---
+++ test/test/Test_CLass.hpp
@@ -0,0 +1,13 @@
+//
+//  Test_CLass.hpp
+//  test
+//
+//  Created by NURIDDINXO'JA ERKINXO'JAYEV on 25/04/22.
+//
+
+#ifndef Test_CLass_hpp
+#define Test_CLass_hpp
+
+#include <stdio.h>
+
+#endif /* Test_CLass_hpp */
